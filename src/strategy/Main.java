eeb4474843d36c35f00b88ce5c2a33aca9ef8d0e
Bruno de Souza Picinini <brunosouzapicinini@gmail.com>
2021-11-21 12:23:38 -0300

    Strategy pattern definition

--- src/strategy/Main.java
+++ src/strategy/Main.java
@@ -8,6 +8,9 @@ import java.util.ArrayList;
 
 public class Main {
 
+    //The Strategy Pattern defines a family of algorithms, encapsulates each one, and makes them interchangeable.
+    //Strategy lets the algorithm vary independently of clients that use it.
+
     public static void main(String[] args) {
         ArrayList<Duck> ducks = new ArrayList<>();
 
