3b62943567ee76d66789c5482f4c9e257bc6690e
Bruno de Souza Picinini <brunosouzapicinini@gmail.com>
2023-06-22 16:20:01 -0300

    rename state main class

--- src/state/Main.java
+++ src/state/Main.java
@@ -3,7 +3,52 @@ package state;
 public class Main {
 
     public static void main(String[] args) {
+        var gumballMachine = new GumBallMachine(5);
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.ejectQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.ejectQuarter();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.refill(1);
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        gumballMachine.refill(2);
+        gumballMachine.insertQuarter();
+        gumballMachine.refill(3);
+        gumballMachine.turnCrank();
+        gumballMachine.refill(4);
+
+        System.out.println(gumballMachine);
 
     }
-
 }
