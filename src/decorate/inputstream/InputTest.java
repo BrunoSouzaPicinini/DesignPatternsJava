bcf925f9ded93b221bb38e5e43a4bb3f8e005e16
Bruno de Souza Picinini <brunosouzapicinini@gmail.com>
2023-06-28 13:44:20 -0300

    Example of a custom concrete decorator of java io lib

---
+++ src/decorate/inputstream/InputTest.java
@@ -0,0 +1,41 @@
+package decorate.inputstream;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InputTest {
+
+    public static void main(String[] args) {
+        int character;
+        try {
+
+            InputStream inputStream = new BufferedInputStream(
+                    new FileInputStream("./src/decorate/inputstream/resources/test.txt"));
+            inputStream.mark(0);
+            while ((character = inputStream.read()) >= 0) {
+                System.out.print((char) character);
+            }
+            inputStream.reset();
+            System.out.println();
+
+            inputStream = new LowerCaseInputStream(inputStream);
+            while ((character = inputStream.read()) >= 0) {
+                System.out.print((char) character);
+            }
+            inputStream.reset();
+            System.out.println();
+
+            inputStream = new UpperCaseInputStream(inputStream);
+            while ((character = inputStream.read()) >= 0) {
+                System.out.print((char) character);
+            }
+
+            inputStream.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
