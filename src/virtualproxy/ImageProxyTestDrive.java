f63b88b4e28cb0f2fc862d4254c0c768ba2dba24
Bruno de Souza Picinini <brunosouzapicinini@gmail.com>
2024-06-10 14:22:05 -0300

    virtual proxy

---
+++ src/virtualproxy/ImageProxyTestDrive.java
@@ -0,0 +1,62 @@
+package virtualproxy;
+
+import java.net.URI;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.*;
+
+
+public class ImageProxyTestDrive {
+    ImageComponent imageComponent;
+    JFrame frame = new JFrame("CD Cover Viewer");
+    JMenuBar menuBar;
+    JMenu menu;
+    Map<String,String> cds = new HashMap<>();
+
+    public static void main(String[] args) throws Exception {
+        ImageProxyTestDrive testDrive = new ImageProxyTestDrive();
+    }
+
+    public ImageProxyTestDrive() throws Exception{
+        cds.put("Buddha Bar","http://images.amazon.com/images/P/B00009XBYK.01.LZZZZZZZ.jpg");
+	    cds.put("Ima","http://images.amazon.com/images/P/B000005IRM.01.LZZZZZZZ.jpg");
+	    cds.put("Karma","http://images.amazon.com/images/P/B000005DCB.01.LZZZZZZZ.gif");
+	    cds.put("MCMXC a.D.","http://images.amazon.com/images/P/B000002URV.01.LZZZZZZZ.jpg");
+	    cds.put("Northern Exposure","http://images.amazon.com/images/P/B000003SFN.01.LZZZZZZZ.jpg");
+	    cds.put("Selected Ambient Works, Vol. 2","http://images.amazon.com/images/P/B000002MNZ.01.LZZZZZZZ.jpg");
+
+        URL initialURL = new URI((String)cds.get("Selected Ambient Works, Vol. 2")).toURL();
+        menuBar = new JMenuBar();
+        menu = new JMenu("Favorite CDs");
+        menuBar.add(menu);
+        frame.setJMenuBar(menuBar);
+
+        for (Object name : cds.keySet()) {
+            JMenuItem menuItem = new JMenuItem((String)name);
+            menu.add(menuItem);
+            menuItem.addActionListener(event -> {
+                var imageProxy = new ImageProxy(getCDUrl(event.getActionCommand()));
+                imageComponent.setIcon(imageProxy);
+                frame.repaint();
+            });
+        }
+
+        Icon icon = new ImageProxy(initialURL);
+        imageComponent = new ImageComponent(icon);
+        frame.getContentPane().add(imageComponent);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,600);
+        frame.setVisible(true);
+    }
+
+
+    private URL getCDUrl(String name) {
+        try {
+            return new URI((String) cds.get(name)).toURL();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
